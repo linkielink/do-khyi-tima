8d821ee0319dd710d3695835c53a047c7cd4487d
Linkie Link <linkielink.dev@gmail.com>
2024-11-20 15:11:21 +0100

    fixed og data

--- src/constants/pageMetadata.ts
+++ src/constants/pageMetadata.ts
@@ -1,51 +1,43 @@
-import { Metadata } from "next";
+import { Metadata } from 'next'
 
-const description = "Description";
-const title = "Do Khyi Tima (The Tibetan Mastiff) - $TIMA";
+const description = 'Description'
+const title = 'Do Khyi Tima (The Tibetan Mastiff) - $TIMA'
 
 export const metaData: { [key: string]: Metadata } = {
   main: {
     title,
-    metadataBase: new URL("https://do-khi-tima.com"),
+    metadataBase: new URL('https://do-khyi-tima.com'),
     description,
-    keywords: [
-      "do khyi",
-      "meme coin",
-      "solana",
-      "crypto",
-      "dog",
-      "mastiff",
-      "tima",
-    ],
+    keywords: ['do khyi', 'meme coin', 'solana', 'crypto', 'dog', 'mastiff', 'tima'],
     openGraph: {
-      type: "website",
-      url: "https://do-khi-tima.com",
+      type: 'website',
+      url: 'https://do-khyi-tima.com',
       title,
-      locale: "en_US",
+      locale: 'en_US',
       description,
-      siteName: "Do Khyi Tima",
+      siteName: 'Do Khyi Tima',
       images: [
         {
-          url: "https://do-khi-tima.com/banner.jpg",
+          url: 'https://do-khyi-tima.com/banner.jpg',
           width: 520,
           height: 275,
-          alt: "Do Khyi Tima",
+          alt: 'Do Khyi Tima',
         },
       ],
     },
     twitter: {
-      card: "summary_large_image",
-      site: "@timasol",
+      card: 'summary_large_image',
+      site: '@timasol',
       title,
       description,
       images: [
         {
-          url: "https://do-khi-tima.com/banner.jpg",
+          url: 'https://do-khyi-tima.com/banner.jpg',
           width: 520,
           height: 275,
-          alt: "Do Khyi Tima",
+          alt: 'Do Khyi Tima',
         },
       ],
     },
   },
-};
+}
