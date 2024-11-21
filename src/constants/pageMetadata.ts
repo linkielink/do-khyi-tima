abf8ef1279363b7a8386245b40cfbebaf88db960
Linkie Link <linkielink.dev@gmail.com>
2024-11-21 18:04:30 +0100

    updated description

--- src/constants/pageMetadata.ts
+++ src/constants/pageMetadata.ts
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
 
-const description = 'Description'
+const description =
+  "TIMA, a mighty Tibetan Mastiff, also known as Do Khyi, is the fiercest guardian in Crypto. These majestic dogs are symbols of strength, loyalty, and wealth. His floofy charm and gentle nature may deceive, but beneath the mane flows the fierce blood of a true 'Heavenly Lion.' TIMA ferociously fends off crypto bears, shielding the pack and guiding hodlers toward prosperity. Join the pack and fight the bears!"
 const title = 'Do Khyi Tima (The Tibetan Mastiff) - $TIMA'
 
 export const metaData: { [key: string]: Metadata } = {
